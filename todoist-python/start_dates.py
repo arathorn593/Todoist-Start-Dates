0ae253e473cf0a928b3b519297c9e6daa949bfee
Joseph Paetz <rjosephp95@gmail.com>
2017-08-16 00:32:25 -0500

    test case work and main loop restructure
    added test cases for parse_sep_date and get_tag_str. Also restructured main loop for start tag.

--- todoist-python/start_dates.py
+++ todoist-python/start_dates.py
@@ -26,6 +26,20 @@ MIN_LEN=2
 LABEL_NAME="Started"
 
 '''
+Finds the date or duration string in an item name
+
+@param name the item name to look for the date/duration string in
+@param the tag that signals the start of the date/duration string
+@returns the date/duration string right after the tag in the item name
+'''
+def get_tag_str(item_name, tag):
+    tag_start = name.find(tag)
+    # if no space before end of line, tag_end=-1 which works
+    tag_end = name.find(" ", tag_start, -1)
+
+    tag_str = name[(tag_start + len(tag)):tag_end]
+
+'''
 Takes a string with a date and returns a date object
 
 @param str the string containing a date
@@ -46,7 +60,7 @@ Takes a string with a date with separators (- or /) and returns a datetime obj
 @returns datetime object with the same date as the string
 '''
 def parse_sep_date(str):
-    pass
+    return datetime.min
 
 '''
 Takes a string with a date in the old format and returns a datetime object
@@ -56,7 +70,18 @@ Takes a string with a date in the old format and returns a datetime object
 @returns datetime object with the same date as the string
 '''
 def parse_old_date(str):
-    pass
+    return datetime.min
+
+'''
+Takes a string with a time duration and returns a timedelta object
+    Ex: 5d1m2y = 5 days 1 month 2 years
+        Month, day, and/or years may be ommitted. An empty string will return
+        a timedelta object of zero
+@param str the string with the duration to convert
+@returns a timedelta object corresponding to the duration given in str
+'''
+def parse_duration(str):
+    return timedelta()
 
 # ------------- MAIN PROGRAM ----------------
 if __name__ == "__main__":
@@ -85,43 +110,30 @@ if __name__ == "__main__":
 
         try:
             error_str = ""
+            tag_str = ""
+            add_label = False
+
             if START_TAG in name:
-                pos = name.find(START_TAG)
-                error_str += "before int conversion\n"
-                day = int(name[(pos+DAY_OFFSET):(pos+DAY_OFFSET+DAY_LEN)])
-                mon = int(name[(pos+MON_OFFSET):(pos+MON_OFFSET+MON_LEN)])
-                year = int(name[(pos+YEAR_OFFSET):(pos+YEAR_OFFSET+YEAR_LEN)])
-                hour = 0
-                minute = 0
-                error_str += "after int conversion\n"
+                now = datetime.today()
+                tag_str = get_tag_str(name, START_TAG)
 
-                has_time = ((pos + TIME_TAG_OFFSET) < len(name) and
-                            name[pos + TIME_TAG_OFFSET] == TIME_TAG)
-                if has_time:
-                    error_str += "before time int conversion\n"
-                    hour = int(name[(pos+HOUR_OFFSET):(pos+HOUR_OFFSET+HOUR_LEN)])
-                    minute = int(name[(pos+MIN_OFFSET):(pos+MIN_OFFSET+MIN_LEN)])
-                    error_str += "after time int conversion\n"
+                item_date = parse_date(tag_str)
 
-                item_id = item['id']
-                item_labels = item['labels']
-                if has_started(day, mon, year, hour, minute, has_time):
-                    item_labels.append(start_label_id)
-                    name = name[0:pos]
-                    api.items.update(item_id, labels=item_labels, content=name)
-                else:
-                    if start_label_id in item_labels:
-                        item_labels.remove(start_label_id)
-                        api.items.update(item_id, labels=item_labels)
-
+                add_label = item_date < now
             elif DURATION_TAG in name:
                 print("huh, this task has a delay tag... wish I could help: %s" %
                       name)
+
+            # Update tag if need be
+            item_id = item['id']
+            item_labels = item['labels']
+            if add_label:
+                item_labels.append(start_label_id)
+                name = name.replace(tag_str, "")
+                api.items.update(item_id, labels=item_labels, content=name)
             else:
-                item_id = item['id']
-                item_labels = item['labels']
-                if item_labels.count(start_label_id) == 0:
-                    item_labels.append(start_label_id)
+                if start_label_id in item_labels:
+                    item_labels.remove(start_label_id)
                     api.items.update(item_id, labels=item_labels)
         except:
             print("task: %s has an invalid start date string" % name)
@@ -131,9 +143,55 @@ if __name__ == "__main__":
 
 
 def run_tests():
-    success = 0
+    passed = True
 
-    print()
+    print("Testing get_tag_str...")
+    inputs_outputs = [("hello \s081217", "\s", "081217"),
+                      ("\d08-1225af hi", "\d", "08-1225af"),
+                      ("test\i52345 hi", "\i", "52345"),
+                      ("test \s12345 test", "\s", "12345")]
+    for io in inputs_outputs:
+        (i0, i1, o) = io
+        ao = get_tag_str(i0, i1)
+        if (ao != o):
+            print("test FAILED: get_tag_str(%s, %s)" % (i0, i1))
+            print("Expected: %s\nActual output:%s" % (o, ao))
+            passed = False
+        else:
+            print("pass: get_tag_str(%s, %s) = %s" % (i0, i1, o))
+    print("\n\n\n")
 
+    print("Testing parse_sep_date...")
+    now = datetime.today()
+    inputs_outputs_sep_date = [("08-17-17", datetime(2017, 8, 17)),
+                               ("8-44", (datetime(now.year, 8, 31) if datetime(now.year, 8, 31) > now else datetime(now.year+1, 8, 31))),
+                               ("9-31-20", datetime(2020, 9, 30)),
+                               ("12-1-18", datetime(2018, 12, 1)),
+                               ("12-08", (datetime(now.year, 12, 8) if datetime(now.year, 12, 8) > now else datetime(now.year+1, 12, 8))),
+                               ("1-1", datetime(now.year+1, 1, 1)),
+                               ("5/6/17", datetime(2017, 5, 6)),
+                               ("7/4", (datetime(now.year, 7, 4) if datetime(now.year, 7, 4) > now else datetime(now.year+1, 12, 8)))]
+    for io in inputs_outputs_sep_date:
+        (i, o) = io
+        ao = parse_sep_date(i)
+        if (ao != o):
+            print("test FAILED: parse_sep_date(%s)" % i)
+            print("Expectd: %s\nActual output:%s" % (o, ao))
+            passed = False
+        else:
+            print("pass: parse_sep_date(%s) = %s" % (i, o))
+    print("\n\n\n")
 
-    return success
+    print("Testing parse_old_date...")
+    print("\n\n\n")
+
+    print("Testing parse_date...")
+    print("\n\n\n")
+
+    print("Testing parse_duration...")
+    print("\n\n\n")
+
+    if passed:
+        return 0
+    else:
+        return 1
